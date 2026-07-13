77d24e6874a6eeb9e00c2d4cd4f4a82d7201815f
Josscoder <jomejiaar@unal.edu.co>
2026-07-13 12:23:46 -0500

    rollback

--- transport/quic.go
+++ transport/quic.go
@@ -46,10 +46,8 @@ func (q *QUIC) Dial(ctx context.Context, addr string) (io.ReadWriteCloser, error
 			},
 			&quic.Config{
 				MaxIdleTimeout:                 time.Second * 10,
-				InitialStreamReceiveWindow:     1024 * 1024 * 8,
-				InitialConnectionReceiveWindow: 1024 * 1024 * 8,
-				MaxIncomingStreams:             200,
-				MaxIncomingUniStreams:          200,
+				InitialStreamReceiveWindow:     1024 * 1024 * 10,
+				InitialConnectionReceiveWindow: 1024 * 1024 * 10,
 				KeepAlivePeriod:                0,
 				InitialPacketSize:              1350,
 				Tracer:                         qlog.DefaultConnectionTracer,
